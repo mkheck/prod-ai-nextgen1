5244136138bd2b9759e618b1b418f92468d653a2
Mark Heckler <mark.heckler@gmail.com>
2024-06-23 19:01:14 -0500

    Added image capabilities, entity conversion

---
+++ src/main/java/com/thehecklers/prod_ai_nextgen1/AIAnswer.java
@@ -0,0 +1,4 @@
+package com.thehecklers.prod_ai_nextgen1;
+
+public record AIAnswer(String content) {
+}
--- src/main/java/com/thehecklers/prod_ai_nextgen1/AIController.java
+++ src/main/java/com/thehecklers/prod_ai_nextgen1/AIController.java
@@ -1,8 +1,11 @@
 package com.thehecklers.prod_ai_nextgen1;
 
+import org.springframework.ai.azure.openai.AzureOpenAiImageModel;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +17,11 @@ import java.util.Map;
 @RequestMapping("/")
 public class AIController {
     private final ChatClient client;
+    private final AzureOpenAiImageModel azureOpenaiImageModel;
 
-    public AIController(ChatClient.Builder builder) {
+    public AIController(ChatClient.Builder builder, AzureOpenAiImageModel azureOpenaiImageModel) {
         this.client = builder.build();
+        this.azureOpenaiImageModel = azureOpenaiImageModel;
     }
 
     @GetMapping
@@ -44,6 +49,14 @@ public class AIController {
                 .chatResponse();
     }
 
+    @GetMapping("/entity")
+    public AIAnswer generateResponseWithEntity(@RequestParam String message) {
+        return client.prompt()
+                .user(message)
+                .call()
+                .entity(AIAnswer.class);
+    }
+
     @GetMapping("/template")
     public String generateResponseFromTemplate(
             @RequestParam String type,
@@ -55,5 +68,14 @@ public class AIController {
         return client.prompt(template.create())
                 .call()
                 .content();
+    }
+
+    @GetMapping("/image")
+    public ImageResponse generateImageResponse(@RequestParam String description) {
+        return azureOpenaiImageModel.call(
+                new ImagePrompt(description)); //,
+//                        AzureOpenAiImageOptions.builder()
+//                                .withHeight(1024)
+//                                .withWidth(1024).build()));
     }
 }
